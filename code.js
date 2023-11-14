92291d2c1f1e27644676b32d09d020759ace3c4c
Agustín Lino <124332751+agustinlinux97@users.noreply.github.com>
2023-11-14 14:40:54 -0300

    Transition

--- code.js
+++ code.js
@@ -1,23 +1,43 @@
 const backgrounds = [
-    "img/background1.jpg",
-    "img/background3.jpg",
-    "img/background4.jpg",
-    "img/background5.jpg",
-    // Agregar aquí todas las imágenes que se quieran usar como fondo
-  ];
-  
-  let currentBackgroundIndex = 0;
-  
-  function changeBackground() {
-    const body = document.getElementsByTagName("body")[0];
-    body.style.backgroundImage = `url(${backgrounds[currentBackgroundIndex]})`;
-  
-    currentBackgroundIndex++;
-  
-    if (currentBackgroundIndex === backgrounds.length) {
-      currentBackgroundIndex = 0;
-    }
+    "img/background1.jpg", "img/background3.jpg", "img/background4.jpg", "img/background5.jpg",
+];
+
+let currentBackgroundIndex = 0;
+
+function changeBackground() {
+  const backgroundsContainer = document.querySelectorAll("#backgroundsContainer")[0];
+  backgroundsContainer.style.backgroundImage = `url(${backgrounds[currentBackgroundIndex]})`;
+
+  currentBackgroundIndex++;
+
+  if (currentBackgroundIndex === backgrounds.length) {
+    currentBackgroundIndex = 0;
   }
+}
   
-  setInterval(changeBackground, 5000); // Cambiar cada 5 segundos (5000 milisegundos)
+  setInterval(changeBackground, 5000);
   
+
+let divParaMostrar = document.getElementById('backgroundsContainer');
+let divParaOcultar = document.getElementById('loader-container');
+
+function mostrarDiv() {
+  divParaMostrar.style.opacity = '1';
+
+
+}
+setTimeout(mostrarDiv, 3000);
+
+function transition() {
+  divParaMostrar.style.display = 'grid'
+}
+setTimeout(transition, 2000)
+
+function ocultarcito() {
+  divParaOcultar.style.display = 'none'
+}
+setTimeout(ocultarcito, 3000)
+function opacitate() {
+  divParaOcultar.style.opacity = '0';
+}
+setTimeout(opacitate, 2000)
